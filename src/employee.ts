87d33007e6259034bd6c5ea159badd2fe4c43ae9
ArvinYang <sugky7302@gmail.com>
2023-02-05 12:50:22 +0000

    完成「新增員工」

--- src/employee.ts
+++ src/employee.ts
@@ -3,5 +3,5 @@ import { PaymentMethod } from "./method/method";
 import { PaymentSchedule } from "./schedule/schedule";
 
 export class Employee {
-    constructor(public readonly name: string, public classification: PaymentClassification, public schedule: PaymentSchedule, public method: PaymentMethod) {}
+    constructor(public readonly empid: number, public readonly name: string, public readonly address: string, public classification: PaymentClassification, public schedule: PaymentSchedule, public method: PaymentMethod) {}
 }
--- src/transaction/add-employee-transaction.ts
+++ src/transaction/add-employee-transaction.ts
@@ -1,11 +1,22 @@
+import { PaymentClassification } from "../classification/classification";
+import { Employee } from "../employee";
+import { HoldMethod } from "../method/hold-method";
+import { PayrollDatabase } from "../payrolldatabase";
+import { PaymentSchedule } from "../schedule/schedule";
 import { Transaction } from "./transaction";
 
-export class AddEmployeeTransaction implements Transaction {
-    constructor(protected empid: number,
-        protected itsAddress: string,
-        protected itsName: string) {
+export abstract class AddEmployeeTransaction implements Transaction {
+    constructor(protected readonly empid: number,
+        protected readonly itsAddress: string,
+        protected readonly itsName: string) {
     }
 
+    protected abstract makeClassification(): PaymentClassification;
+    protected abstract makeSchedule(): PaymentSchedule;
+
+    // 執行新增員工的事務
     public execute(): void {
+        const e = new Employee(this.empid, this.itsName, this.itsAddress, this.makeClassification(), this.makeSchedule(), new HoldMethod());
+        PayrollDatabase.addEmployee(this.empid, e);
     }
 }
